027a1962f376f18a079d0643b482acf8b9ebf1ee
Ahdaaa <afilzag@gmail.com>
2023-10-30 16:45:34 +0700

    feat: ajax on submit

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
-function validateForm(){
-    const email = document.forms["form1"]["Username"].value;
-    const pass = document.forms["form1"]["Password"].value;
-
-    if (email == "" || pass == ""){
-        alert("Please fill all the required data");
-    } 
-    else {
-        alert("Success to login");
+$(function () {
+    function sendData($form) {
+      let data = $(this).serialize();
+      return $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: data,
+      });
     }
-
-}
+    
+    $("form").validate();
+    
+    $("form").submit(function (e) {
+        e.preventDefault();
+    
+        sendData($(this)).done(function(){
+          $("#container_form").html("<div id='message' style='background-color:linear-gradient(rgb(26, 26, 26) 0%, rgb(0, 0, 0) 100%); text-align:center; color:#FFFFFF;'></div>");
+          $("#container_form").css({
+            'height': '85%',
+            'display': 'flex',
+            'justify-content': 'center',
+            'align-items': 'center'
+          });
+          $("#message")
+            .html("<h2>Form Submitted Succesfully</h2>")
+            .append("<p>We will be in touch soon.</p>")
+            .hide()
+            .fadeIn(1500);
+        });
+    });
+  });
